05c9127c22496f15ac074491051dce08a8f343d1
JamesGarcia <james_1805@hotmail.com>
2020-03-20 16:28:06 -0500

    Creacion Login Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CircleTapComponent } from './circle-tap/circle-tap.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo : 'login', pathMatch : 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'circletap', component: CircleTapComponent },
+  { path: '**', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,16 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.inicio();
   }
+
+  public inicio(){
+    setTimeout(() => {
+      $(".imgInicio").css({"display":"none"})
+      $(".login").css({"display":"flex"})
+    }, 3000);
+  }
+
+  
 
 }
