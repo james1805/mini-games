ab8dc91dc4a595e22711cf9cd6cd5161fb77f94d
JamesGarcia <james_1805@hotmail.com>
2020-03-20 13:08:14 -0500

    Creacion de circleTap v1

--- src/app/circle-tap/circle-tap.component.ts
+++ src/app/circle-tap/circle-tap.component.ts
@@ -7,9 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CircleTapComponent implements OnInit {
 
+  public ejeX:number = Math.floor(Math.random() * (window.innerWidth-(window.innerWidth*0.25))) + 1;
+  public ejeY:number = Math.floor(Math.random() * (window.innerHeight-(window.innerHeight*0.25))) + 1;
+  public puntaje:number = 0;
+  public contador:number = 50;
+  public sizeScreenWidth:number;  
+  public sizeScreenHeight:number;
+  public time:number = 50;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    this.sizeScreenWidth = window.innerWidth;
+    this.sizeScreenHeight = window.innerHeight;   
+    this.drawFigure(this.contador);
+    this.timeClock(this.time)
+    
+  }
+
+  public timeClock(time){
+    setTimeout(() => {
+      if (time>0) {
+        time -= 1
+        this.time = time;
+        this.timeClock(time);
+      }      
+    }, 1000);
+    if(time == 0){
+      console.log('GAME OVER');
+    }
+  }
+
+  public drawFigure(contador){    
+    $(".tapButton").css({"margin-left": `${this.ejeX}px`, "margin-top": `${this.ejeY}px`});
+      
+    setTimeout(() => {
+      if(contador > 0){  
+     
+        this.ejeX = Math.floor(Math.random() * (this.sizeScreenWidth - (this.sizeScreenWidth * 0.25))) + 1;
+        this.ejeY = Math.floor(Math.random() * (this.sizeScreenHeight - (this.sizeScreenHeight * 0.25))) + 1; 
+        $(".tapButton").css({"margin-left": `${this.ejeX}px`, "margin-top": `${this.ejeY}px`});      
+      
+        contador = contador - 1;
+      
+        this.drawFigure(contador);
+      }
+    }, 1000);
+  }
+  
+  public tap(){
+    if(this.time > 0){
+      this.puntaje += 1;
+    }
+        
   }
 
 }
